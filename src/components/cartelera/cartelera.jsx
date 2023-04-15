db0497c1338e59775dc1e75b5291a527ff364e67
jeisonmr <jeisonmzr@gmail.com>
2023-04-15 14:54:37 -0500

    interfaz lista de peliculas

--- src/components/cartelera/cartelera.jsx
+++ src/components/cartelera/cartelera.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BannersMovie } from "../banners/bannerMovie";
 import Data from "../../assets/catalogo.json";
-import Options from '../../assets/options.json';
+import Options from "../../assets/options.json";
 import "./cartelra.css";
 import "../banners/bannerMovie";
 
@@ -9,14 +9,13 @@ export const CineList = () => {
   return (
     <main className="containerListCartelera">
       <div className="searchSection">
-        <input className="searchInput" type="searchInput" />
+        
+        <input className="searchInput" type="search" />
         <select className="selectInput" name="" id="" placeholder="Genero">
           {Options.map((i) => {
-            return (
-            i.Genero.map((i) => {
-              return (<option value={i}>{i}</option>)
-            })
-            )
+            return i.Genero.map((i) => {
+              return <option value={i}>{i}</option>;
+            });
           })}
         </select>
       </div>
@@ -24,16 +23,22 @@ export const CineList = () => {
       <br />
       <hr />
       <div className="bannerSection">
-        {Data.map((i) => {
-          return (
-            <div className="marcoBanner">
-              <BannersMovie img={i.banner} title={i.titulo} />
-              <div className="description">
-                <p>{i.titulo}</p>
-                {i.genero.map(i => <span className="des">{i}</span>)}
-              </div>
-            </div>
-          );
+        {Data.map((img) => {
+          return img.genero.map((genero) => {
+            if (genero === "aventura") {
+              return (
+                <div className="marcoBanner">
+                  <BannersMovie img={img.banner} title={img.titulo} />
+                  <div className="description">
+                    <p>{img.titulo}</p>
+                    {img.genero.map((i) => (
+                      <span className="des">{i}</span>
+                    ))}
+                  </div>
+                </div>
+              );
+            }
+          });
         })}
       </div>
     </main>
--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
-import imgFirst from '/public/img/sliders/banner-avg.png';
-import imgSecond from '/public/img/sliders/banner-promo.png';
+import imgBanner_1 from '/public/img/sliders/banner-avg.png';
+import imgBanner_2 from '/public/img/sliders/banner-promo.png';
+import imgBanner_3 from '/public/img/sliders/colfilms.png';
+import imgBanner_4 from '/public/img/sliders/dc.png';
+import imgBanner_5 from '/public/img/sliders/encanto.png';
+import imgBanner_6 from '/public/img/sliders/la-monja.png';
+import imgBanner_7 from '/public/img/sliders/marvel.png';
 export const Slider = () => {
   return (
       <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src={imgFirst} className="d-block w-100" alt="..."/>
+          <img src={imgBanner_1} className="d-block w-100" alt="..."/>
         </div>
         <div className="carousel-item">
-          <img src={imgSecond} className="d-block w-100" alt="..."/>
+          <img src={imgBanner_2} className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src={imgBanner_3} className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src={imgBanner_4} className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src={imgBanner_5} className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src={imgBanner_6} className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src={imgBanner_7} className="d-block w-100" alt="..."/>
         </div>
       </div>
       <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
